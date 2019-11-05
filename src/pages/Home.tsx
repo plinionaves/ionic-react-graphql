f0703f5abd92a373280b6359c4f63dd245360489
Plinio Naves <pliniopjn@hotmail.com>
2019-11-05 13:43:57 -0300

    GraphQL Code Generator: tipagem automática no TypeScript

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,9 +10,10 @@ import React from 'react'
 import { useQuery } from '@apollo/react-hooks'
 
 import { LAUNCHES_PAST_QUERY } from '../graphql/launches'
+import { LaunchesPastQuery } from '../generated/graphql'
 
 const Home: React.FC = () => {
-  const { data, loading } = useQuery(LAUNCHES_PAST_QUERY)
+  const { data, loading } = useQuery<LaunchesPastQuery>(LAUNCHES_PAST_QUERY)
 
   return (
     <IonPage>
@@ -26,9 +27,9 @@ const Home: React.FC = () => {
           <p>Loading...</p>
         ) : (
           data &&
-          data.launchesPast.map((launch: any) => (
-            <IonItem key={launch.id}>
-              {launch.mission_name} | {launch.rocket.rocket_name}
+          data.launchesPast!.map(launch => (
+            <IonItem key={launch!.id as string}>
+              {launch!.mission_name} | {launch!.rocket!.rocket_name}
             </IonItem>
           ))
         )}
