5e7b2b2c07fe9b3a4e6762f51ac2ad65e04d95bd
Plinio Naves <pliniopjn@hotmail.com>
2019-11-05 10:52:41 -0300

    Configurando Apollo Client via Context API

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import App from './App'
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css'
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css'
+import '@ionic/react/css/structure.css'
+import '@ionic/react/css/typography.css'
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css'
+import '@ionic/react/css/float-elements.css'
+import '@ionic/react/css/text-alignment.css'
+import '@ionic/react/css/text-transformation.css'
+import '@ionic/react/css/flex-utils.css'
+import '@ionic/react/css/display.css'
+
+/* Theme variables */
+import './theme/variables.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
