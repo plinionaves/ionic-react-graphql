49d935ff5980f05cb30c2db91e0cf080dd057d7c
Plinio Naves <pliniopjn@hotmail.com>
2019-11-05 22:44:14 -0300

    LaunchesItem: estilizando com CSS Modules

--- src/components/LaunchesItem.tsx
+++ src/components/LaunchesItem.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { IonCard, IonImg } from '@ionic/react'
 
 import { Launch } from '../generated/graphql'
+import styles from './LaunchesItem.module.scss'
 
 interface Props {
   launch: Launch
@@ -11,10 +12,10 @@ const LaunchesItem: React.FC<Props> = props => {
   const { launch } = props
 
   return (
-    <IonCard button>
-      <IonImg src={launch.links.flickr_images[0]} />
-      <h2>{launch.mission_name}</h2>
-      <p>{launch.rocket.rocket_name}</p>
+    <IonCard button className={styles.card}>
+      <IonImg src={launch.links.flickr_images[0]} className={styles.img} />
+      <h2 className={styles.cardTitle}>{launch.mission_name}</h2>
+      <p className={styles.cardSubtitle}>{launch.rocket.rocket_name}</p>
     </IonCard>
   )
 }
