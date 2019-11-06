9d3f6241ce9f4ea7e918ee3b81c713e5ba8cb93b
Plinio Naves <pliniopjn@hotmail.com>
2019-11-06 14:53:33 -0300

    Função crop para cortar textos por palavras

--- src/components/LaunchesItem.tsx
+++ src/components/LaunchesItem.tsx
@@ -3,6 +3,7 @@ import { IonCard, IonImg } from '@ionic/react'
 
 import { Launch } from '../generated/graphql'
 import styles from './LaunchesItem.module.scss'
+import { crop } from '../utils'
 
 interface Props {
   launch: Launch
@@ -14,7 +15,7 @@ const LaunchesItem: React.FC<Props> = props => {
   return (
     <IonCard button className={styles.card}>
       <IonImg src={launch.links.flickr_images[0]} className={styles.img} />
-      <h2 className={styles.cardTitle}>{launch.mission_name}</h2>
+      <h2 className={styles.cardTitle}>{crop(launch.mission_name, 15)}</h2>
       <p className={styles.cardSubtitle}>{launch.rocket.rocket_name}</p>
     </IonCard>
   )
