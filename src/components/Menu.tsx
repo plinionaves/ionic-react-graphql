65da8899a3866cc288725e1584760ae62e87704b
Plinio Naves <pliniopjn@hotmail.com>
2019-11-08 19:51:17 -0300

    Links para as páginas do App no Menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,17 +1,42 @@
 import React from 'react'
-import { IonMenu, IonHeader, IonToolbar, IonTitle } from '@ionic/react'
+import {
+  IonMenu,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonIcon,
+  IonText,
+  IonMenuToggle,
+} from '@ionic/react'
 import { Components } from '@ionic/core'
+import { locate } from 'ionicons/icons'
+
+interface Link {
+  path: string
+  title: string
+  icon: any
+  direction: 'none' | 'forward' | 'back'
+}
 
 interface Props extends Partial<Components.IonMenu> {
   title?: string
   toolbarColor?: string
+  links?: Link[]
 }
+
+const defaultLinks: Link[] = [
+  { path: '/launches', title: 'Launches', icon: locate, direction: 'back' },
+]
 
 const Menu: React.FC<Props> = props => {
   const {
     title = 'Menu',
     toolbarColor = 'tertiary',
     contentId = 'main-content',
+    links = defaultLinks,
     ...rest
   } = props
 
@@ -22,6 +47,23 @@ const Menu: React.FC<Props> = props => {
           <IonTitle>{title}</IonTitle>
         </IonToolbar>
       </IonHeader>
+      <IonContent>
+        <IonList lines="none">
+          <IonMenuToggle menu={props.menuId}>
+            {links.map(link => (
+              <IonItem
+                key={link.path}
+                button
+                routerLink={link.path}
+                routerDirection={link.direction}
+              >
+                <IonIcon icon={link.icon} color={toolbarColor} slot="start" />
+                <IonText color={toolbarColor}>{link.title}</IonText>
+              </IonItem>
+            ))}
+          </IonMenuToggle>
+        </IonList>
+      </IonContent>
     </IonMenu>
   )
 }
